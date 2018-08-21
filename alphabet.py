8d272d1415237ff9764a8fca90f73355f2f68afe
jabestinamalan4 <42029675+jabestinamalan4@users.noreply.github.com>
2018-08-21 11:34:08 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,6 @@
+a=["a","e","i","o","u","b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+c=input("Input:")
+if c in a:
+    print("Alphabet")
+else:
+    print("No")
